580ea93edff58b782f53e03e7fe3a3ef0ee489ac
zin koko <zinkoko5211@gmail.com>
2025-05-01 23:43:26 +0900

    day7/02

--- day7/day7step2.py
+++ day7/day7step2.py
@@ -5,17 +5,29 @@ chosen_word = random.choice(word_list)
 
 placeholder = '_'*len(chosen_word)
 placeholderLst = list(placeholder)
+life = 6
+guessed_Lst = []
 
-while '_' in placeholderLst:
+while '_' in placeholderLst and life > 0:
     guess = input("Guess the letter: ").lower()
+    if not(guess in guessed_Lst) and guess in chosen_word:
+        guessed_Lst.append(guess)
+        cnt = 0
+        for letter in chosen_word:
+            if guess == letter:
+                placeholderLst[cnt] = letter
+            cnt += 1
 
-    cnt = 0
-    for letter in chosen_word:
-        if guess == letter:
-            placeholderLst[cnt] = letter
-        cnt += 1
+        placeholder = ''
+        for letter in placeholderLst:
+            placeholder += letter
+        print(placeholder)
+        print(guessed_Lst)
+        print(f"life = {life}/6")
+    else:
+        life -= 1
+        print(placeholder)
+        print(guessed_Lst)
+        print(f"life = {life}/6")
 
-    placeholder = ''
-    for letter in placeholderLst:
-        placeholder += letter
-    print(placeholder)
+
