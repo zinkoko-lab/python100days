4da9ed33cc8769abcb2c159bc33f88128e0c00c0
zin koko <zinkoko5211@gmail.com>
2025-05-07 23:33:26 +0900

    day8/07

--- day8/caesar.py
+++ day8/caesar.py
@@ -31,43 +31,43 @@ def encrypt(plain_text: str, key: int):
         Returns:
             str: エンコードされた文字列
     """
-    encoded_text = ''
+    cipher_text = ''
     for letter in plain_text:
         if letter.isalpha():    # アルファベットの場合のみデコード処理を行う
             # 大文字の場合
             if letter.isupper():
-                encoded_text += chr(ord('A') + (ord(letter) - ord('A') + key) % 26)     # シフト後の文字を計算
+                cipher_text += chr(ord('A') + (ord(letter) - ord('A') + key) % 26)     # シフト後の文字を計算
             # 小文字の場合
             else:
-                encoded_text += chr(ord('a') + (ord(letter) - ord('a') + key) % 26)
+                cipher_text += chr(ord('a') + (ord(letter) - ord('a') + key) % 26)
         # アルファベット以外はそのまま追加
         else:
-            encoded_text += letter
+            cipher_text += letter
 
-    return encoded_text
+    return cipher_text
 
-def decrypt(encoded_text: str, key: int):
+def decrypt(cipher_text: str, key: int):
     """
         シーザー暗号をデコードする。
 
         Args:
-            encoded_text (str): デコード対象の文字列
+            cipher_text (str): デコード対象の文字列
             key (int): シフトする文字数
 
         Returns:
             str: デコードされた文字列
     """
-    decoded_text = ''
-    for letter in encoded_text:
+    plain_text = ''
+    for letter in cipher_text:
         if letter.isalpha():
             if letter.isupper():
-                decoded_text += chr(ord('Z') - (ord('Z') - ord(letter) + key) % 26)
+                plain_text += chr(ord('Z') - (ord('Z') - ord(letter) + key) % 26)
             else:
-                decoded_text += chr(ord('z') - (ord('z') - ord(letter) + key) % 26)
+                plain_text += chr(ord('z') - (ord('z') - ord(letter) + key) % 26)
         else:
-            decoded_text += letter
+            plain_text += letter
 
-    return decoded_text
+    return plain_text
 
 
 # メインループ
