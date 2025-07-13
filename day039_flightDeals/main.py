f8a3cf5950376e1b2e2c6807cd5b572140d69dc3
zin koko <zinkoko5211@gmail.com>
2025-07-13 10:32:21 +0900

    day38/2

--- day039_flightDeals/main.py
+++ day039_flightDeals/main.py
@@ -1,1 +1,4 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# amadeus api key and api secrect
+AMAEUS_API_KEY = La2ofXDnEZeeLX5E8b1PaeEncBd3Q6iG
+AMAEUS_API_SECRET = FVUwPHQwnSVoLxEl
